1bad6f64e6554e52439a54bb4a7de1e7e49dd8be
Pepijn Bakker <bakker.pepijn@gmail.com>
2022-09-14 15:32:29 +0200

    converted template 3

--- NIEWE TEMPLATES/03_Basics/HelloWorld/Properties/AssemblyInfo.cs
+++ NIEWE TEMPLATES/03_Basics/HelloWorld/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BasicGame")]
-[assembly: AssemblyProduct("BasicGame")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTitle("HelloWorld")]
 [assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyCopyright("Copyright © 2018")]
+[assembly: AssemblyProduct("HelloWorld")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ab6af394-b346-4200-82c6-0a5f5f2261eb")]
+[assembly: Guid("a453e342-e608-433f-ad62-cb9839eecee1")]
 
 // Version information for an assembly consists of the following four values:
 //
